bd1a38ca574167d880419d89f457bc181e4c1965
Christopher Broschard <cbroschard@hotmail.com>
2026-05-29 16:39:50 -0400

    main: added boost options/skeleton config

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,29 @@
 // non-commercial use only. Redistribution, modification, or use
 // of this code in whole or in part for any other purpose is
 // strictly prohibited without the prior written consent of the author.
+#include <boost/program_options.hpp>
+#include <fstream>
 #include <iostream>
 
-using namespace std;
+namespace po = boost::program_options;
+
+po::options_description get_config_file_options()
+{
+    po::options_description desc("Configuration File Options");
+    desc.add_options()
+        ("BIOS", po::value<std::string>()->required(), "ColecoVision BIOS");
+               ;
+    return desc;
+}
 
 int main()
 {
+    // Process configuration file, exit if there are any errors as we won't know how to boot the system
+    std::ifstream configFile("colecovision.cfg");
+    if (!configFile)
+    {
+        std::cerr << "Error: Unable to open configuration file colecovision.cfg exiting!" << std::endl;
+        return 1;
+    }
     return 0;
 }
