ee8d15c6583a0c918f57cbffc549dc586404f26a
Christopher Broschard <cbroschard@hotmail.com>
2026-05-30 21:21:50 -0400

    Bus: stubbed out readIO/writeIO for BIOS load testing

--- src/Bus.cpp
+++ src/Bus.cpp
@@ -8,7 +8,6 @@
 #include "Bus.h"
 
 Bus::Bus() :
-    cart(nullptr),
     controller1(nullptr),
     controller2(nullptr),
     mem(nullptr),
@@ -21,4 +20,30 @@ Bus::Bus() :
 Bus::~Bus()
 {
 
+}
+
+uint8_t Bus::readMemory(uint16_t address)
+{
+    if (!mem)
+        return 0xFF;
+
+    return mem->read(address);
+}
+
+void Bus::writeMemory(uint16_t address, uint8_t value)
+{
+    if (!mem)
+        return;
+
+    mem->write(address, value);
+}
+
+uint8_t Bus::readIO(uint8_t port)
+{
+    return 0xFF;
+}
+
+void Bus::writeIO(uint8_t port, uint8_t value)
+{
+    // TODO: route to VDP, PSG, controllers
 }
