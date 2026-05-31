6ecf285cbe3349fda22fdb48431943d74ed84016
Christopher Broschard <cbroschard@hotmail.com>
2026-05-30 21:21:36 -0400

    main: Implemented basic bios configuration and system

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <boost/program_options.hpp>
 #include <fstream>
 #include <iostream>
+#include "ColecoVisionSystem.h"
 
 namespace po = boost::program_options;
 
@@ -16,18 +17,62 @@ po::options_description get_config_file_options()
     po::options_description desc("Configuration File Options");
     desc.add_options()
         ("BIOS", po::value<std::string>()->required(), "ColecoVision BIOS");
-               ;
     return desc;
 }
 
 int main()
 {
-    // Process configuration file, exit if there are any errors as we won't know how to boot the system
-    std::ifstream configFile("colecovision.cfg");
-    if (!configFile)
+    try
     {
-        std::cerr << "Error: Unable to open configuration file colecovision.cfg exiting!" << std::endl;
+        // Build our system
+        ColecoVisionSystem system;
+
+        // Process configuration file, exit if there are any errors as we won't know how to boot the system
+        std::ifstream configFile("colecovision.cfg");
+        if (!configFile)
+        {
+            std::cerr << "Error: Unable to open configuration file colecovision.cfg exiting!" << std::endl;
+            return 1;
+        }
+
+        po::variables_map vmConfig;
+        po::options_description configFileOptions = get_config_file_options();
+
+        // Validate required options before moving on
+        try
+        {
+            po::store(po::parse_config_file(configFile, configFileOptions), vmConfig);
+            po::notify(vmConfig);
+        }
+        catch (const po::unknown_option& e)
+        {
+            std::cerr << "Unknown option encountered: " << e.what() << std::endl;
+            return 1;
+        }
+        catch (const po::required_option& e)
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return 1;
+        }
+
+        system.setBIOSPath(vmConfig["BIOS"].as<std::string>());
+        system.run();
+
+        return 0;
+    }
+    catch (const std::runtime_error& error)
+    {
+        std::cerr << "Error: Runtime error exception: " << error.what() << std::endl;
         return 1;
     }
-    return 0;
+    catch (const std::exception& error)
+    {
+        std::cerr << "Error: General exception: " << error.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Error: Caught an unknown exception!" << std::endl;
+        return 1;
+    }
 }
