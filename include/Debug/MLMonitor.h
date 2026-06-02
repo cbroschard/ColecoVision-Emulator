4221688d45870695a6731bec9e8925ba9ca3631d
Christopher Broschard <cbroschard@hotmail.com>
2026-06-01 21:33:19 -0400

    ML Monitor: Skeleton class for future monitor

---
+++ include/Debug/MLMonitor.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2026 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef MLMONITOR_H
+#define MLMONITOR_H
+
+
+class MLMonitor
+{
+    public:
+        MLMonitor();
+        virtual ~MLMonitor();
+
+    protected:
+
+    private:
+};
+
+#endif // MLMONITOR_H
